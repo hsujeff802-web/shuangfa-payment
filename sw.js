e98e7a9c83a728bcdc2178ab4049542b2b69663b
hsujeff802-web <hsujeff802@gmail.com>
2026-08-19 16:45:38 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME='shuangfa-payment-v8.3-buildrc0288-internal-logout-backup';
+const CACHE_NAME='shuangfa-payment-v8.3-buildrc0289-internal-logout-backup';
 const APP_SHELL=[
   './',
   './index.html',
-  './styles.css?v=83288',
-  './app.js?v=83288','./v83.js?v=83288',
+  './styles.css?v=83289',
+  './app.js?v=83289','./v83.js?v=83289',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
