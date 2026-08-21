1098fdc84afcb65d251e21b4f61452ef911a6f76
hsujeff802-web <hsujeff802@gmail.com>
2026-08-21 11:32:23 +0800

    Add files via upload

--- v83.js
+++ v83.js
@@ -2145,7 +2145,6 @@
     if (typeof hydrateFromIndexedDB === 'function') await hydrateFromIndexedDB();
     if (typeof createOpeningBackup === 'function') await createOpeningBackup();
     licenseReady = await ensureLicense();
-    if (licenseReady) hideLicenseGate();
     renderLicenseInfo();
     syncLoginBrand();
     const systemInfo = q('#systemInfoCard .backup-status');
